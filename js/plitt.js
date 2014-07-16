2c45047c42f8d9ea8b4959ee4498c10cbfad5dc5
ryenus <ryenus@gmail.com>
2014-07-16 13:24:06 +0800

    Update plitt.js

--- js/plitt.js
+++ js/plitt.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @name  PLITT
+// @version    1.0
 // @namespace  ryenus.toys
 // @include    http://www.google.tld/*
 // @include    https://www.google.tld/*
@@ -7,20 +8,19 @@
 // @include    https://www.google.co.tld/*
 // @include    http://www.google.com.tld/*
 // @include    https://www.google.com.tld/*
-// @Description allow visiting search result without the redirection overhead
+// @description allow visiting search result without the redirection overhead
 // ==/UserScript==
 
 (function(d, t) {
   if (! (d.forms[0] && d.forms[0].action.match(/search/))) return;
   d.addEventListener('DOMSubtreeModified', function() {
     if (t == 0) t = setTimeout(function() {
-      var r = d.getElementsByClassName('r');
-      if (r.length > d.getElementsByClassName('plitt').length) {
-        for (var i = 0; i < r.length; i++) {
-          var l = r[i].lastChild;
-          //if (l.previousElementSibling) continue;
+      var r = d.getElementsByClassName('r'), n = r.length;
+      if (n > d.getElementsByClassName('plitt').length) {
+        while(n--) {
+          var l = r[n].lastChild;
           l.insertAdjacentHTML("beforebegin",
-            "<a href='" + l.href + "' target='_blank' class='plitt'>[#]</a>&nbsp;");
+            "<a href='" + l.href + "' target='_blank' class='plitt'>[#]</a> ");
         }
       }
       t = 0;
