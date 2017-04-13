c0f74d4853d2e7bab862fb460f96a6bda1164284
ryenus <roger.ye@sap.com>
2017-04-14 00:33:59 +0800

    clean up looping logic

--- js/plitt.js
+++ js/plitt.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name  PLITT
-// @version    1.0
+// @version    1.1
 // @namespace  ryenus.toys
 // @include    http://www.google.tld/*
 // @include    https://www.google.tld/*
@@ -9,20 +9,19 @@
 // @include    http://www.google.com.tld/*
 // @include    https://www.google.com.tld/*
 // @description allow visiting search result without the redirection overhead
+// @grant      none
 // ==/UserScript==
 
 (function(d, t) {
   if (! (d.forms[0] && d.forms[0].action.match(/search/))) return;
   d.addEventListener('DOMSubtreeModified', function() {
-    if (t == 0) t = setTimeout(function() {
-      var r = d.getElementsByClassName('r'), n = r.length;
-      if (n > d.getElementsByClassName('plitt').length) {
-        while(n--) {
-          var l = r[n].lastChild;
-          l.insertAdjacentHTML("beforebegin",
-            "<a href='" + l.href + "' target='_blank' class='plitt'>[#]</a> ");
-        }
-      }
+    if (t === 0) t = setTimeout(function() {
+      Array.from(d.getElementsByClassName('r')).forEach(function(r, i) {
+          if (r.children.length == 1) {
+              r.insertAdjacentHTML("afterbegin",
+                "<a href='" + r.lastChild.href + "' target='_blank' class='plitt'>[#" + i +"]</a>&nbsp;");
+          }
+      });
       t = 0;
     }, 500);
   }, false);
